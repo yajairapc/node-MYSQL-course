c48e6ac1ef46a84f879a2c9d5de290fbcaba83c6
Yajaira Pierrez <yajairapc376@gmail.com>
2024-11-27 14:41:12 -0600

    initial commit

--- result.js
+++ result.js
@@ -12,14 +12,8 @@ con.connect(function(err){
 	if(err) throw err;
 	console.log("Connected to the database");
 
-	//Insert into
-	var sql="INSERT INTO customers(name, email) VALUES ?";
-	var values= [
-		['Abi Perez', 'abisaip.rw@gmail.com'],
-		['Mikel Perez', 'mikel@gmail.com'],
-		['Damaris Carrillo', 'dama.pc95@gmail.com'],
-	]
-	con.query(sql, [values], function (err, result){
+	var sql= "INSERT INTO customers (name, email) VALUES ('steve','steve@gmail.com')";
+	con.query(sql, function (err, result){
 		if(err) throw err;
 		console.log("Records inserted " + result.affectedRows);
 	});
